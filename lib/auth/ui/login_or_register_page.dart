fd013ccfe3dfb04330668d14c84b6774ba8d4ad7
VinhLam <lamthevinh2606@gmail.com>
2025-09-28 15:59:02 +0700

    add lib

---
+++ lib/auth/ui/login_or_register_page.dart
@@ -0,0 +1,33 @@
+// lib/auth/ui/login_or_register_page.dart
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+import 'register_page.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  // ban đầu, hiển thị trang đăng nhập
+  bool showLoginPage = true;
+
+  // phương thức để chuyển đổi giữa hai trang
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
